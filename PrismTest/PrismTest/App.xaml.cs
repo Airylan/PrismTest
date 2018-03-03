3d3f51e36a707648a64de77cc88fb060cc438fc3
Mike MacMartin <mmacmartin@gmail.com>
2018-03-03 13:56:41 -0500

    Hooking up Prism and the Unity Bootstrapper.

--- PrismTest/PrismTest/App.xaml.cs
+++ PrismTest/PrismTest/App.xaml.cs
@@ -16,6 +16,9 @@ namespace PrismTest
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
         }
     }
 }
---
+++ PrismTest/PrismTest/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using PrismTest.Views;
+using PrismTest.ViewModels;
+// using PrismTest.Entities;
+// using PrismTest.Services;
+
+namespace PrismTest
+{
+    internal class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell() => Container.Resolve<MainWindowView>();
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterType<MainWindowView>();
+        }
+    }
+}
